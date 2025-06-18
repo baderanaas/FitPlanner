d4f6d066399fe4425484e31a2d644e11eb267726
Anas Bader <anas.bader@insat.ucar.tn>
2025-06-18 17:53:38 +0100

    fixed the redirection from login to register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/clerk-react";
 export default function Login() {
   return (
     <div className="d-flex w-100 h-100">
-      <SignIn signInUrl="/register" forceRedirectUrl={"/chat"}/>
+      <SignIn signUpUrl="/register" forceRedirectUrl={"/chat"}/>
     </div>
   );
 }
